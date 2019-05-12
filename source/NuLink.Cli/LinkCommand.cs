933f0846b86726839081de2c319871be967212da
Felix Berman <felix-b@users.noreply.github.com>
2019-05-12 22:10:14 +0300

    Improve reliability of ‘link’ command (#14)

--- source/NuLink.Cli/LinkCommand.cs
+++ source/NuLink.Cli/LinkCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using Murphy.SymbolicLink;
 
 namespace NuLink.Cli
@@ -12,43 +13,83 @@ namespace NuLink.Cli
             Console.WriteLine(
                 $"Checking package references in {(options.ProjectIsSolution ? "solution" : "project")}: {options.ConsumerProjectPath}");
 
-            var allProjects = new WorkspaceLoader().LoadProjects(options.ConsumerProjectPath, options.ProjectIsSolution);
-            var referenceLoader = new PackageReferenceLoader();
-            var allPackages = referenceLoader.LoadPackageReferences(allProjects);
-
-            var requestedPackage = allPackages.FirstOrDefault(p => p.PackageId == options.PackageId);
-
-            if (requestedPackage == null)
-            {
-                throw new Exception($"Error: Package not referenced: {options.PackageId}");
-            }
-
+            var requestedPackage = GetPackageInfo();
             var status = requestedPackage.CheckStatus();
-
-            if (!status.LibFolderExists)
-            {
-                throw new Exception($"Error: Cannot link package {options.PackageId}: 'lib' folder not found, expected {requestedPackage.LibFolderPath}");
-            }
-
-            if (status.IsLibFolderLinked)
-            {
-                throw new Exception($"Error: Package {requestedPackage.PackageId} is already linked to {status.LibFolderLinkTargetPath}");
-            }
-
-            if (!status.LibBackupFolderExists)
-            {
-                Directory.Move(requestedPackage.LibFolderPath, requestedPackage.LibBackupFolderPath);
-            }
-            else
-            {
-                Console.WriteLine($"Warning: backup folder was not expected to exist: {requestedPackage.LibBackupFolderPath}");
-            }
-
             var linkTargetPath = Path.Combine(Path.GetDirectoryName(options.LocalProjectPath), "bin", "Debug");
-            CreateSymbolicLink(linkPath: requestedPackage.LibFolderPath, targetPath: linkTargetPath);
+
+            ValidateOperation();
+            PerformOperation();
             
             Console.WriteLine($"Linked {requestedPackage.LibFolderPath} -> {linkTargetPath}");
             return 0;
+
+            PackageReferenceInfo GetPackageInfo()
+            {
+                var allProjects = new WorkspaceLoader().LoadProjects(options.ConsumerProjectPath, options.ProjectIsSolution);
+                var referenceLoader = new PackageReferenceLoader();
+                var allPackages = referenceLoader.LoadPackageReferences(allProjects);
+                var package = allPackages.FirstOrDefault(p => p.PackageId == options.PackageId);
+                return package ?? throw new Exception($"Error: Package not referenced: {options.PackageId}");
+            }
+            
+            void ValidateOperation()
+            {
+                if (!status.LibFolderExists)
+                {
+                    throw new Exception($"Error: Cannot link package {options.PackageId}: 'lib' folder not found, expected {requestedPackage.LibFolderPath}");
+                }
+
+                if (status.IsLibFolderLinked)
+                {
+                    throw new Exception($"Error: Package {requestedPackage.PackageId} is already linked to {status.LibFolderLinkTargetPath}");
+                }
+
+                if (!Directory.Exists(linkTargetPath))
+                {
+                    throw new Exception($"Error: Target link directory doesn't exist: {linkTargetPath}");
+                }
+            }
+
+            void PerformOperation()
+            {
+                if (!status.LibBackupFolderExists)
+                {
+                    Directory.Move(requestedPackage.LibFolderPath, requestedPackage.LibBackupFolderPath);
+                }
+                else
+                {
+                    Console.WriteLine($"Warning: backup folder was not expected to exist: {requestedPackage.LibBackupFolderPath}");
+                }
+
+                try
+                {
+                    CreateSymbolicLink(linkPath: requestedPackage.LibFolderPath, targetPath: linkTargetPath);
+                }
+                catch
+                {
+                    RevertOperation();
+                    throw;
+                }
+            }
+
+            void RevertOperation()
+            {
+                try
+                {
+                    Console.WriteLine("Failed to create symlink, reverting changes to package folders.");
+                    Directory.Move(requestedPackage.LibBackupFolderPath, requestedPackage.LibFolderPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"FAILED to revert package changes: {e.Message}");
+                    Console.WriteLine($"You have to recover manually!");
+                    Console.WriteLine("--- MANUAL RECOVERY INSTRUCTIONS ---");
+                    Console.WriteLine($"1. Go to {Path.GetDirectoryName(requestedPackage.LibFolderPath)}");
+                    Console.WriteLine($"2. Rename '{Path.GetFileName(requestedPackage.LibBackupFolderPath)}'" +
+                                      $" to '{Path.GetFileName(requestedPackage.LibFolderPath)}'");
+                    Console.WriteLine("--- END OF RECOVERY INSTRUCTIONS ---");
+                }
+            }
         }
 
         private void CreateSymbolicLink(string linkPath, string targetPath)
