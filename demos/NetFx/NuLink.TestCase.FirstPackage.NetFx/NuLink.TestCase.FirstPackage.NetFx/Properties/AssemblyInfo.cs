e700785601958e1a90852e15a8d692673c32bb2a
Felix Berman <felixberman76@gmail.com>
2019-08-11 09:33:18 +0300

    Add NetFx suffix to all folders and files of NetFx test case

--- demos/NetFx/NuLink.TestCase.FirstPackage.NetFx/NuLink.TestCase.FirstPackage.NetFx/Properties/AssemblyInfo.cs
+++ demos/NetFx/NuLink.TestCase.FirstPackage.NetFx/NuLink.TestCase.FirstPackage.NetFx/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuLink.TestCase.FirstPackage")]
+[assembly: AssemblyTitle("NuLink.TestCase.FirstPackage.NetFx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuLink.TestCase.FirstPackage")]
+[assembly: AssemblyProduct("NuLink.TestCase.FirstPackage.NetFx")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- demos/NetFx/NuLink.TestCase.SecondPackage.NetFx/NuLink.TestCase.SecondPackage.NetFx/Properties/AssemblyInfo.cs
+++ demos/NetFx/NuLink.TestCase.SecondPackage.NetFx/NuLink.TestCase.SecondPackage.NetFx/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuLink.TestCase.SecondPackage")]
+[assembly: AssemblyTitle("NuLink.TestCase.SecondPackage.NetFx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuLink.TestCase.SecondPackage")]
+[assembly: AssemblyProduct("NuLink.TestCase.SecondPackage.NetFx")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
