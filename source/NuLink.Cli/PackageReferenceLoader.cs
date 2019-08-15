1e1e8e75c2917c003d69187a0b1bdda4d4a7f894
Felix Berman <felixberman76@gmail.com>
2019-08-15 09:13:26 +0300

    Make PackageReferenceLoader polymorphic for NetCore/NetFx - WIP

--- source/NuLink.Cli/PackageReferenceLoader.cs
+++ source/NuLink.Cli/PackageReferenceLoader.cs
@@ -36,69 +36,148 @@ namespace NuLink.Cli
         
         public IEnumerable<PackageReferenceInfo> LoadPackageReferences(ProjectAnalyzer project)
         {
-            var packagesRootFolder = GetPackagesRootFolder(project);
-            var packages = GetPackages();
+            var referenceLoader = ReferenceLoader.Create(_ui, project);
+            return referenceLoader.LoadPackageReferences();
+        }
 
-            return packages.Where(p => p != null);
-                        
-            IEnumerable<PackageReferenceInfo> GetPackages()
+        private abstract class ReferenceLoader
+        {
+            protected ReferenceLoader(IUserInterface ui, ProjectAnalyzer project, XElement projectXml)
             {
-                var csprojXml = XElement.Load(project.ProjectFile.Path);
-                var elements = csprojXml.XPathSelectElements("//PackageReference");
-            
-                return elements.Select(e => {
-                    var packageId = e.Attribute("Include")?.Value;
-                    var version = e.Attribute("Version")?.Value;
-
-                    if (!string.IsNullOrWhiteSpace(packageId) && !string.IsNullOrWhiteSpace(version))
-                    {
-                        var folder = GetPackageFolder(packageId, version);
-                        return new PackageReferenceInfo(packageId, version, folder);
-                    }
-
-                    return null;
-                });
+                this.UI = ui;
+                this.Project = project;
+                this.ProjectXml = projectXml;
             }
 
-            string GetPackageFolder(string packageId, string version)
-            {
-                var packageFolderPath = Path.Combine(
-                    packagesRootFolder,
-                    packageId.ToLower(),
-                    version.ToLower());
+            public abstract IEnumerable<PackageReferenceInfo> LoadPackageReferences();
 
-                return packageFolderPath;
+            protected IUserInterface UI { get; }
+
+            protected ProjectAnalyzer Project { get; }
+
+            protected XElement ProjectXml { get; }
+
+            public static ReferenceLoader Create(IUserInterface ui, ProjectAnalyzer project)
+            {
+                var projectXml = XElement.Load(project.ProjectFile.Path);
+
+                var isSdkStyle = SdkStyleReferenceLoader.IsSdkStyleProject(projectXml);
+                var isOldStyle = OldStyleReferenceLoader.IsOldStyleProject(projectXml);
+
+                if (isSdkStyle && !isOldStyle)
+                {
+                    return new SdkStyleReferenceLoader(ui, project, projectXml);
+                }
+
+                if (isOldStyle && !isSdkStyle)
+                {
+                    return new OldStyleReferenceLoader(ui, project, projectXml);
+                }
+                
+                throw new Exception($"Error: could not recognize project format: {project.ProjectFile.Path}");
             }
         }
 
-        private string GetPackagesRootFolder(ProjectAnalyzer project)
+        private class SdkStyleReferenceLoader : ReferenceLoader
         {
-            var ns = new XmlNamespaceManager(new NameTable());
-            ns.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003");
-
-            var nugetPropsXml = XElement.Load(GetNuGetPropsFilePath(project));
-            var result = 
-                nugetPropsXml.XPathSelectElement("//msb:NuGetPackageRoot", ns)?.Value
-                ?? throw new Exception("Could not find NuGetPackageRoot property");
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            public SdkStyleReferenceLoader(IUserInterface ui, ProjectAnalyzer project, XElement projextXml)
+                : base(ui, project, projextXml)
             {
-                var userProfilePath = Environment.GetEnvironmentVariable("UserProfile");
-                _ui.ReportLow(() => $"Detected Windows: $(UserProfile)=[{userProfilePath}]");
-                result = result.Replace("$(UserProfile)", userProfilePath, StringComparison.InvariantCultureIgnoreCase);
             }
-            
-            return result;
+
+            public override IEnumerable<PackageReferenceInfo> LoadPackageReferences()
+            {
+                var packagesRootFolder = GetPackagesRootFolder();
+                var packages = GetPackages();
+    
+                return packages.Where(p => p != null);
+                            
+                IEnumerable<PackageReferenceInfo> GetPackages()
+                {
+                    var elements = ProjectXml.XPathSelectElements("//PackageReference");
+                
+                    return elements.Select(e => {
+                        var packageId = e.Attribute("Include")?.Value;
+                        var version = e.Attribute("Version")?.Value;
+    
+                        if (!string.IsNullOrWhiteSpace(packageId) && !string.IsNullOrWhiteSpace(version))
+                        {
+                            var folder = GetPackageFolder(packageId, version);
+                            return new PackageReferenceInfo(packageId, version, folder);
+                        }
+    
+                        return null;
+                    });
+                }
+    
+                string GetPackageFolder(string packageId, string version)
+                {
+                    var packageFolderPath = Path.Combine(
+                        packagesRootFolder,
+                        packageId.ToLower(),
+                        version.ToLower());
+    
+                    return packageFolderPath;
+                }
+            }
+    
+            private string GetPackagesRootFolder()
+            {
+                var ns = new XmlNamespaceManager(new NameTable());
+                ns.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003");
+    
+                var nugetPropsXml = XElement.Load(GetNuGetPropsFilePath());
+                var result = 
+                    nugetPropsXml.XPathSelectElement("//msb:NuGetPackageRoot", ns)?.Value
+                    ?? throw new Exception("Could not find NuGetPackageRoot property");
+    
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    var userProfilePath = Environment.GetEnvironmentVariable("UserProfile");
+                    UI.ReportLow(() => $"Detected Windows: $(UserProfile)=[{userProfilePath}]");
+                    result = result.Replace("$(UserProfile)", userProfilePath, StringComparison.InvariantCultureIgnoreCase);
+                }
+                
+                return result;
+            }
+    
+            private string GetNuGetPropsFilePath()
+            {
+                var filePath = Path.Combine(
+                    Path.GetDirectoryName(Project.ProjectFile.Path),
+                    "obj",
+                    $"{Path.GetFileName(Project.ProjectFile.Path)}.nuget.g.props");
+    
+                return filePath;
+            }
+
+            public static bool IsSdkStyleProject(XElement projectXml)
+            {
+                var sdkValue = projectXml.Attribute("Sdk")?.Value;
+                return !string.IsNullOrEmpty(sdkValue);
+            }
         }
 
-        private static string GetNuGetPropsFilePath(ProjectAnalyzer project)
+        private class OldStyleReferenceLoader : ReferenceLoader
         {
-            var filePath = Path.Combine(
-                Path.GetDirectoryName(project.ProjectFile.Path),
-                "obj",
-                $"{Path.GetFileName(project.ProjectFile.Path)}.nuget.g.props");
+            public const string MsbuildNamespaceName = "http://schemas.microsoft.com/developer/msbuild/2003";
+            
+            public OldStyleReferenceLoader(
+                IUserInterface ui, ProjectAnalyzer project, XElement projectXml) 
+                : base(ui, project, projectXml)
+            {
+            }
 
-            return filePath;
+            public override IEnumerable<PackageReferenceInfo> LoadPackageReferences()
+            {
+                throw new NotImplementedException();
+            }
+
+            public static bool IsOldStyleProject(XElement projectXml)
+            {
+                var defaultNamespace = projectXml.GetDefaultNamespace();
+                return (defaultNamespace.NamespaceName == MsbuildNamespaceName);
+            }
         }
     }
 }
